d7d2b6f8b07a5293d896ca4a851547698b139e13
Josh Stallnick <joshua.nickleby@gmail.com>
2022-07-31 12:59:34 -0500

    Up build om logging

--- simpleKafka/build.gradle.kts
+++ simpleKafka/build.gradle.kts
@@ -15,11 +15,14 @@ dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-autoconfigure-processor")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.springframework.kafka:spring-kafka")
-  implementation("io.github.microutils:kotlin-logging-jvm:2.1.20")
+  implementation("io.github.microutils:kotlin-logging-jvm:2.1.23")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.kafka:spring-kafka-test")
   testImplementation("org.testcontainers:kafka:1.17.3")
+}
+repositories {
+  mavenCentral()
 }
 
 tasks.withType<Test> {
