f7d4d0eee2a3737c85fe7f8a96a28d1ba9ecad34
Josh Stallnick <violabs.software@gmail.com>
2022-12-12 02:30:09 -0600

    Initial setup of postgres example
    - Docker file
    - Gradle and application files

---
+++ postgres/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("org.springframework.boot")
+
+    kotlin("jvm")
+    kotlin("plugin.spring")
+}
+
+dependencies {
+    implementation(project(":core"))
+
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    annotationProcessor("org.springframework.boot:spring-boot-autoconfigure-processor")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,8 @@ val modules: Map<Int, String> = mapOf(
   0 to "core",
   1 to "first",
   2 to "simpleKafka",
-  3 to "kafkaConnect"
+  3 to "kafkaConnect",
+  4 to "postgres"
 )
 
 modules.values.forEach(::include)
