dc217f492c1bd9b43ae50c09724ff2af04c6db11
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2024-04-13 20:02:45 -0500

    weaviate

--- weaviate/build.gradle.kts
+++ weaviate/build.gradle.kts
@@ -7,24 +7,26 @@ plugins {
     id("com.avast.gradle.docker-compose") version "0.17.6"
 }
 
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+}
+
 dependencies {
     implementation(project(":core"))
-    implementation(project(":core:springJpaCore"))
 
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     annotationProcessor("org.springframework.boot:spring-boot-autoconfigure-processor")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
-
-    runtimeOnly("org.postgresql:postgresql")
+    implementation("io.springboot.ai:spring-ai-ollama-spring-boot-starter:1.0.1")
+    implementation("io.springboot.ai:spring-ai-weaviate-store:1.0.0")
+    implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
+	implementation("org.springdoc:springdoc-openapi-webmvc-core:1.8.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-repositories {
-    mavenCentral()
 }
 
 dockerCompose {
---
+++ weaviate/src/test/kotlin/io/violabs/weaviate/WeaviateApplicationTest.kt
@@ -0,0 +1,13 @@
+package io.violabs.weaviate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WeaviateApplicationTest {
+
+    @Test
+    fun `context loads`() {
+        assert(true)
+    }
+}
