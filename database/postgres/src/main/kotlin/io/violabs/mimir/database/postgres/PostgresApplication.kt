5dcc62b44496f9ffe762e34768f24c73b4628627
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2025-02-06 22:51:09 -0600

    Add component scan.

--- database/postgres/src/main/kotlin/io/violabs/mimir/database/postgres/PostgresApplication.kt
+++ database/postgres/src/main/kotlin/io/violabs/mimir/database/postgres/PostgresApplication.kt
@@ -2,10 +2,12 @@ package io.violabs.mimir.database.postgres
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = ["io.violabs.mimir.core.springJpaCore"])
+@ComponentScan(basePackages = ["io.violabs.mimir.core.springJpaCore", "io.violabs.mimir.database.postgres"])
 class PostgresApplication
 
 fun main(args: Array<String>) {
